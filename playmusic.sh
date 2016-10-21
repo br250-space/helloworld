d4ac0e47f036c7b56668cba58965731f6efa3dd7
br250.space <canddy.zhang.do@gmail.com>
2016-10-21 19:08:11 +0800

    p2

---
+++ playmusic.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash 
+#
+#  you  add  spotify  source into ubuntu source list
+#
+
+#  tee  | sed 
+# show you how to append  record to need root right file 
+# normal append record  use >> op but some stuff you can't do ac
+
+# use apt install spotify-client  you os 
+
+
+# if os == ubuntu  check *.desktop file 
+# where *.desktop  localtion ---> /usr/share/applications  
+# where     /usr/share/spotify --> spotify.desktop  you can check it
+
+# just check one options --> Exec   
+# push file to you ui tools 
+
+
+
+# ubuntu 16.06 gungpg make key fail 
+# check you machine exists hw random 
+# cat /proc/sys/kernel/random/entropy_avail >1000 
+# install Haveged  
+# no good random no good password
+
+
+
