7d45532a3aef1c7e880972a0562ee68166397fa5
br250.space <canddy.zhang.do@gmail.com>
2016-10-21 14:13:00 +0800

    add some cli to helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash 
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+echo `whoami`
+
